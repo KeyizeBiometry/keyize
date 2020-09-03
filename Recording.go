ebeb4844f6dfac21fe404df40859178a9b73b540
ethanent <futuristiccake@gmail.com>
2020-09-02 23:48:53 -0700

    keyize/Recording: create Recording type and related types

---
+++ Recording.go
@@ -0,0 +1,22 @@
+package keyize
+
+import "time"
+
+type RawEventKind int
+
+const (
+	KeyDown RawEventKind = iota
+	KeyUp
+)
+
+// Recording represents a user's raw typing recording
+type Recording struct {
+	Events []RecordingEvent
+}
+
+// RecordingEvent is a specific event which took place during a recording
+type RecordingEvent struct {
+	At   time.Time
+	Kind RawEventKind
+	Subject rune
+}
